0a89024a4c8beedfb0bf1a0db10b08bf5d493bff
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2022-07-07 18:54:43 -0700

    cleaned up formatting

--- scripts/backup-any-folder.sh
+++ scripts/backup-any-folder.sh
@@ -9,9 +9,8 @@ source='/path/to/source'
 
 # Set backup directory
 destination='/path/to/destination'
-# Set Number of Days to Keep Backups 
+# Set Number of Days to Keep Backups
 delete_after=2
-
 
 #------------- DO NOT MODIFY BELOW THIS LINE -------------#
 # Will not run again if currently running.
@@ -19,10 +18,10 @@ if [ -e "/tmp/i.am.running.${name}" ]; then
     echo "Another instance of the script is running. Aborting."
     exit
 else
-    touch  "/tmp/i.am.running.${name}"
+    touch "/tmp/i.am.running.${name}"
 fi
 
-start=`date +%s`	# start time of script for statistics
+start=$(date +%s) # start time of script for statistics
 cd "$(realpath -s $source)"
 
 dest=$(realpath -s $destination)/
@@ -31,7 +30,7 @@ dt=$(date +"%m-%d-%Y")
 # create the backup directory if it doesn't exist - error handling - will not create backup file it path does not exist
 mkdir -p "$dest"
 # Creating backup of directory
-echo -e  "\n\nCreating backup... please wait"
+echo -e "\n\nCreating backup... please wait"
 mkdir -p "$dest/$dt"
 
 #Script Data Backup
@@ -42,16 +41,15 @@ sleep 2
 chmod -R 777 "$dest"
 
 #Cleanup Old Backups
-echo -e  "\n\nRemoving backups older than " $delete_after "days... please wait\n\n"
+echo -e "\n\nRemoving backups older than " $delete_after "days... please wait\n\n"
 find $destination* -mtime +$delete_after -exec rm -rfd {} \;
 
-
-end=`date +%s`
+end=$(date +%s)
 #Finish
-echo -e  "\nTotal time for backup: " $((end-start)) "seconds\n"
-echo -e  '\nAll Done!\n'
+echo -e "\nTotal time for backup: " $((end - start)) "seconds\n"
+echo -e '\nAll Done!\n'
 if [ -d $dest/ ]; then
-	echo -e  Total size of all backups: "$(du -sh $dest/)"
+    echo -e Total size of all backups: "$(du -sh $dest/)"
 fi
 rm "/tmp/i.am.running.${name}"
 exit

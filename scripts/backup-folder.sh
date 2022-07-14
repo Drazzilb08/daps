f0bcc04289f939101c249a05c655347b4f2516c1
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2022-07-14 16:37:43 -0700

    Discord overhaul/cleanup

--- scripts/backup-folder.sh
+++ scripts/backup-folder.sh
@@ -1,77 +1,69 @@
 #!/bin/bash
 
-#       ____             _                  ______    _     _           
-#      |  _ \           | |                |  ____|  | |   | |          
-#      | |_) | __ _  ___| | ___   _ _ __   | |__ ___ | | __| | ___ _ __ 
+#       ____             _                  ______    _     _
+#      |  _ \           | |                |  ____|  | |   | |
+#      | |_) | __ _  ___| | ___   _ _ __   | |__ ___ | | __| | ___ _ __
 #      |  _ < / _` |/ __| |/ / | | | '_ \  |  __/ _ \| |/ _` |/ _ \ '__|
-#      | |_) | (_| | (__|   <| |_| | |_) | | | | (_) | | (_| |  __/ |   
-#      |____/ \__,_|\___|_|\_\\__,_| .__/  |_|  \___/|_|\__,_|\___|_|   
-#                                  | |                                  
-#                                  |_|                                  
-
+#      | |_) | (_| | (__|   <| |_| | |_) | | | | (_) | | (_| |  __/ |
+#      |____/ \__,_|\___|_|\_\\__,_| .__/  |_|  \___/|_|\__,_|\___|_|
+#                                  | |
+#                                  |_|
 
 #------------- DEFINE VARIABLES -------------#
-name=''                                 # Set your script name, must be unique to any other script.
-source=''                               # Set source directory
-destination=''                          # Set backup directory
-delete_after=2                          # Number of days to keep backup
-usePigz=yes                             # Use pigz to further compress your backup (yes) will use pigz to further compress, (no) will not use pigz
-                                            # Pigz package must be installed via NerdPack
-pigzCompression=9						# Define compression level to use with pigz
-                                            # 0 = No compression
-                                            # 1 = Least compression/Fastest
-                                            # 6 = Default compression/Default Speed
-                                            # 9 = Maximum Compression/Slowest
-notify=yes                              # Use unRAID's built in notification system
+name=''                                             # Set your script name, must be unique to any other script.
+source=''                                           # Set source directory
+destination=''                                      # Set backup directory
+delete_after=2                                      # Number of days to keep backup
+use_pigz=yes                                        # Use pigz to further compress your backup (yes) will use pigz to further compress, (no) will not use pigz
+                                                        # Pigz package must be installed via NerdPack
+pigz_compression=9                                  # Define compression level to use with pigz
+                                                        # 0 = No compression
+                                                        # 1 = Least compression/Fastest
+                                                        # 6 = Default compression/Default Speed
+                                                        # 9 = Maximum Compression/Slowest
+notify=yes                                          # Use unRAID's built in notification system
 #------------- DEFINE DISCORD VARIABLES -------------#
-# This section is not required
-# This portion requires discord.sh to be downloaded and placed somewhere in a location accessable by this script
-# You can find discord.sh ----> https://github.com/ChaoticWeg/discord.sh
-# Simply download or clone the repo and extract the discord.sh file to a loaction then define that location in the discordLoc variable
-
-useDiscord=yes                          # Use discord for notifications
-discordLoc=''                           # Full location to discord.sh
-											# Eg. '/mnt/user/data/scripts/discord.sh'
-webhook=''                              # Discord webhook
-botName='Notification Bot'              # Name your bot
-titleName='Server Notifications'        # Give a title name to your discord messages
-barColor='FFFFFF'                       # The bar color for discord notifications, must be Hexcode
-avatarUrl=''                            # Url for the avatar you want your bot to have
+                                                    # This section is not required
+use_discord=yes                                     # Use discord for notifications
+webhook='' # Discord webhook
+bot_name='Notification Bot'                         # Name your bot
+bar_color='3036236'                                 # The bar color for discord notifications, must be decimal -> https://www.mathsisfun.com/hexadecimal-decimal-colors.html
 
 #------------- DO NOT MODIFY BELOW THIS LINE -------------#
 # Will not run again if currently running.
 if [ ! -d "$source" ]; then
-	echo "ERROR: Your source directory does not exist, please check your configuration"
-	exit
+    echo "ERROR: Your source directory does not exist, please check your configuration"
+    exit
 fi
 if [ -z "$source" ]; then
-    echo "ERROR: Your source directory is not set , please check your configuration"
-	exit
+    echo "ERROR: Your source directory is not set, please check your configuration."
+    exit
 fi
-if [ "$useDiscord" == "yes" ] && [ ! -f "$discordLoc" ] || [ -z "$discordLoc" ]; then
-	echo "ERROR: You're attempting to use the Discord integration but discord.sh is not found at ${discordLoc} or not set"
-	exit
+if [ "$use_discord" == "yes" ] && [ -z "$webhook" ]; then
+    echo "ERROR: You're attempting to use the Discord integration but did not enter the webhook url."
+    exit
 fi
-if [ command -v pigz &> /dev/null ] && [ "$usePigz" == "yes"]; then 
+if [ command -v pigz ] &>/dev/null && [ "$use_pigz" == "yes"]; then
     echo "pigz could not be found."
     echo "Please install pigz and rerun."
     echo "If on unRaid, pigz can be found through the NerdPack which is found in the appstore"
     exit
 fi
-if [ -e "/tmp/i.am.running.${name}" ]; then
+if [ -e "/tmp/i.am.running.${name}.tmp" ]; then
     echo "Another instance of the script is running. Aborting."
-    echo "Please use rm /tmp/i.am.running.${name} in your terminal to remove the locking file"
+    echo "Please use rm /tmp/i.am.running.${name}.tmp in your terminal to remove the locking file"
     exit
 else
-    touch "/tmp/i.am.running.${name}"
+    touch "/tmp/i.am.running.${name}.tmp"
 fi
 
 #Set variables
-start=$(date +%s)                   #Sets start time for runtime information
+start=$(date +%s)
 cd "$(realpath -s $source)"
 dest=$(realpath -s $destination)/
 dt=$(date +"%m-%d-%Y")
 now=$(date +"%I_%M_%p")
+get_ts=$(date -u -Iseconds)
 
 # create the backup directory if it doesn't exist - error handling - will not create backup file it path does not exist
 mkdir -p "$dest"
@@ -81,11 +73,11 @@ mkdir -p "$dest/$dt"
 
 # Data Backup
 tar -cf "$dest/$dt/backup-"$now".tar" "$source"
-runsize=$(du -sh $dest/$dt/backup-"$now".tar | awk '{print $1}') #Set runsize information
-if [ $usePigz == yes ]; then
+run_size=$(du -sh $dest/$dt/backup-"$now".tar | awk '{print $1}') #Set run_size information
+if [ $use_pigz == yes ]; then
     echo -e "\nUsing pigz to compress backup... this could take a while..."
-    pigz -$pigzCompression "$dest/$dt/backup-"$now".tar"
-    runsize=$(du -sh $dest/$dt/backup-"$now".tar.gz | awk '{print $1}')
+    pigz -$pigz_compression "$dest/$dt/backup-"$now".tar"
+    run_size=$(du -sh $dest/$dt/backup-"$now".tar.gz | awk '{print $1}')
 fi
 
 sleep 2
@@ -97,51 +89,40 @@ find $destination* -mtime +$delete_after -exec rm -rfd {} \;
 
 end=$(date +%s)
 # Runtime
-totalTime=$((end - start))
-seconds=$((totalTime % 60))
-minutes=$((totalTime % 3600 / 60))
-hours=$((totalTime / 3600))
+total_time=$((end - start))
+seconds=$((total_time % 60))
+minutes=$((total_time % 3600 / 60))
+hours=$((total_time / 3600))
 
 if (("$minutes" == "0" && "$hours" == "0")); then
     echo "Script completed in $seconds seconds"
-    runOutput="Script completed in $seconds seconds"
+    run_output="Script completed in $seconds seconds"
 elif (("$hours" == "0")); then
     echo "Script completed in $minutes minutes and $seconds seconds"
-    runOutput="Script completed in $minutes minutes and $seconds seconds"
+    run_output="Script completed in $minutes minutes and $seconds seconds"
 else
     echo "Script completed in $hours hours $minutes minutes and $seconds seconds"
-    runOutput="Script completed in $hours hours $minutes minutes and $seconds seconds"
+    run_output="Script completed in $hours hours $minutes minutes and $seconds seconds"
 fi
 
 # Gather size information
-echo -e "This backup's size: $runsize"
+echo -e "This backup's size: $run_size"
 if [ -d $dest/ ]; then
-    totalsize=$(du -sh $dest | awk '{print $1}')
-    echo -e "\nTotal size of all backups: $totalsize"
+    total_size=$(du -sh $dest | awk '{print $1}')
+    echo -e "\nTotal size of all backups: $total_size"
 fi
-# Removing temp file
-rm "/tmp/i.am.running.${name}"
 
 # Notifications
 if [ "$notify" == "yes" ]; then
     /usr/local/emhttp/webGui/scripts/notify -e "Unraid Server Notice" -s "${name} Backup" -d "Backup completed: ${name} data has been backed up." -i "normal"
 fi
-if [ "$useDiscord" == "yes" ]; then
-    ${discordLoc} --webhook-url="$webhook" --username "${botName}" \
-        --title "${titleName}" \
-        --avatar "$avatarUrl" \
-        --description "Backup completed: ${name} data has been backed up." \
-        --field "Runtime; $runOutput.;false" \
-        --field "This backup's size:; $runsize;false" \
-        --field "Total size of all backups:; ${totalsize};false" \
-        --color "0x$barColor" \
-        --footer "Powered by: Drazzilb" \
-        --footer-icon "https://i.imgur.com/r69iYhr.png" \
-        --timestamp
+if [ "$use_discord" == "yes" ]; then
+    curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${bot_name}"'","embeds": [{"title": "Appdata Backup Complete","description": "Backup completed: Pictures data has been backed up.","fields": [{"name": "Runtime:","value": "'"${run_output}"'"},{"name": "'"This Backup's size:"'","value": "'"${run_size}"'"},{"name": "Total size of all backups:","value": "'"${total_size}"'"}],"footer": {"text": "Powered by: Drazzilb | Could he be more heartless?","icon_url": "https://i.imgur.com/r69iYhr.png"},"color": "'"${bar_color}"'","timestamp": "'"${get_ts}"'"}]}' "$webhook"
     echo -e "\nDiscord notification sent."
 fi
 echo -e '\nAll Done!\n'
-
+# Removing temp file
+rm "/tmp/i.am.running.${name}.tmp"
 exit
-#  
-# v1.0.2
+#
+# v1.1.2

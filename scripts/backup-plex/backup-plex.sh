5eae5a58f9560660ccc81f102affbab8d2e6f0b7
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2022-08-02 11:16:59 -0700

    v2.3.11
    - Minor changes from last commit
    - Reworked backup funciton that was causing issues with certain directories with spaces in them

--- scripts/backup-plex/backup-plex.sh
+++ scripts/backup-plex/backup-plex.sh
@@ -11,7 +11,7 @@
 # v2.3.11
 
 # Define where your config file is located
-config_file='backup-plex.conf'
+config_file=''
 
 #------------- DO NOT MODIFY BELOW THIS LINE -------------#
 debug=no # Testing Only
@@ -68,8 +68,11 @@ script_setup_function() {
             lastbackup=0
         fi
     fi
-    # $(realpath -s $source) takes care of the presence or absense of a trailing slash (/) on the source path - error handling
-    cd "$(realpath -s "$source")" || exit
+    # $(realpath -s $source) takes care of the presence or absense of a trailing slash (/) on the source path - error handling\
+    parentdir=$(dirname "$source")
+    cd "$(realpath -s "$parentdir")" || exit
+    source_basename=$(basename "$source")
+
     # set the destination directory and a date
     dest=$(realpath -s "$destination")/
     if [ "$alternate_format" == "yes" ]; then
@@ -83,43 +86,55 @@ script_setup_function() {
     mkdir -p "$dest"
 }
 backup_function() {
-# create tar file of essential databases and preferences -- The Plug-in Support preferences will keep settings of any plug-ins, even though they will need to be reinstalled.
-echo -e "Creating $3 backup... please wait"
-mkdir -p "$dest/$3/$dt"
-if [ "$debug" != yes ]; then
-    if [ "$alternate_format" != "yes" ]; then
-        $1
-        size=$(du -sh "$dest/$3/$dt/$3_Plex_Data_Backup-$now.tar" | awk '{print $1}')
-        if [ "$use_pigz" == yes ]; then
-            pigz -$pigz_compression "$dest/$3/$dt/$3_Plex_Data_Backup-$now.tar"
-            size=$(du -sh "$dest/$3/$dt/$3_Plex_Data_Backup-$now.tar.gz" | awk '{print $1}')
+    # create tar file of essential databases and preferences -- The Plug-in Support preferences will keep settings of any plug-ins, even though they will need to be reinstalled.
+    echo -e "Creating $1 backup... please wait"
+    mkdir -p "$dest/$1/$dt"
+    if [ "$debug" != yes ]; then
+        if [ "$alternate_format" != "yes" ]; then
+            if [ "$cf" == false ]; then
+                echo "use me 1"
+                tar -cf "$dest/Essential/$dt/Essential_Plex_Data_Backup-$now.tar" "$source_basename/Plug-in Support/Databases" "$source_basename/Plug-in Support/Preferences" "$source_basename/Preferences.xml"
+            else
+                echo "use me 2"
+                tar --exclude="$source_basename"/Cache --exclude="$source_basename"/Codecs -cf "$dest/Full/$dt/Full_Plex_Data_Backup-$now.tar" "$source_basename"
+            fi
+            size=$(du -sh "$dest/$1/$dt/$1_Plex_Data_Backup-$now.tar" | awk '{print $1}')
+            if [ "$use_pigz" == yes ]; then
+                pigz -$pigz_compression "$dest/$1/$dt/$1_Plex_Data_Backup-$now.tar"
+                size=$(du -sh "$dest/$1/$dt/$1_Plex_Data_Backup-$now.tar.gz" | awk '{print $1}')
+            fi
+        else
+            if [ "$cf" == false ]; then
+                echo "use me 3"
+                tar -cf "$dest/Essential/$dt/Essential_Plex_Data_Backup.tar" "Plug-in Support/Databases" "Plug-in Support/Preferences" "Preferences.xml"
+            else
+                echo "use me 4"
+                tar --exclude="$source_basename"/Cache --exclude="$source_basename"/Codecs -cf "$dest/Full/$dt/Full_Plex_Data_Backup.tar $source_basename" "$source_basename"
+            fi
+            size=$(du -sh "$dest/$1/$dt/$1_Plex_Data_Backup.tar" | awk '{print $1}')
+            if [ "$use_pigz" == yes ]; then
+                pigz -$pigz_compression "$dest/$1/$dt/$1_Plex_Data_Backup.tar"
+                size=$(du -sh "$dest/$1/$dt/$1_Plex_Data_Backup.tar.gz" | awk '{print $1}')
+            fi
         fi
     else
-        $2
-        size=$(du -sh "$dest/$3/$dt/$3_Plex_Data_Backup.tar" | awk '{print $1}')
-        if [ "$use_pigz" == yes ]; then
-            pigz -$pigz_compression "$dest/$3/$dt/$3_Plex_Data_Backup.tar"
-            size=$(du -sh "$dest/$3/$dt/$3_Plex_Data_Backup.tar.gz" | awk '{print $1}')
+        if [ "$alternate_format" != "yes" ]; then
+            tar -cf "$dest/$1/$dt/$1_Plex_Data_Backup-debug0-$now.tar" -T /dev/null
+            size=$(du -sh "$dest/$1/$dt/$1_Plex_Data_Backup-debug0-$now.tar" | awk '{print $1}')
+            if [ "$use_pigz" == yes ]; then
+                pigz -$pigz_compression "$dest/$1/$dt/$1_Plex_Data_Backup-debug0-$now.tar"
+                size=$(du -sh "$dest/$1/$dt/$1_Plex_Data_Backup-debug0-$now.tar.gz" | awk '{print $1}')
+            fi
+        else
+            tar -cf "$dest/$1/$dt/$1_Plex_Data_Backup-debug0.tar" -T /dev/null
+            size=$(du -sh "$dest/$1/$dt/$1_Plex_Data_Backup-debug0.tar" | awk '{print $1}')
+            if [ "$use_pigz" == yes ]; then
+                pigz -$pigz_compression "$dest/$1/$dt/$1_Plex_Data_Backup-debug0.tar"
+                size=$(du -sh "$dest/$1/$dt/$1_Plex_Data_Backup-debug0.tar.gz" | awk '{print $1}')
+            fi
         fi
     fi
-else
-    if [ "$alternate_format" != "yes" ]; then
-        tar -cf "$dest/$3/$dt/$3_Plex_Data_Backup-debug0-$now.tar" -T /dev/null
-        size=$(du -sh "$dest/$3/$dt/$3_Plex_Data_Backup-debug0-$now.tar" | awk '{print $1}')
-        if [ "$use_pigz" == yes ]; then
-            pigz -$pigz_compression "$dest/$3/$dt/$3_Plex_Data_Backup-debug0-$now.tar"
-            size=$(du -sh "$dest/$3/$dt/$3_Plex_Data_Backup-debug0-$now.tar.gz" | awk '{print $1}')
-        fi
-    else
-        tar -cf "$dest/$3/$dt/$3_Plex_Data_Backup-debug0.tar" -T /dev/null
-        size=$(du -sh "$dest/$3/$dt/$3_Plex_Data_Backup-debug0.tar" | awk '{print $1}')
-        if [ "$use_pigz" == yes ]; then
-            pigz -$pigz_compression "$dest/$3/$dt/$3_Plex_Data_Backup-debug0.tar"
-            size=$(du -sh "$dest/$3/$dt/$3_Plex_Data_Backup-debug0.tar.gz" | awk '{print $1}')
-        fi
-    fi
-fi
-echo -e "Done\n"
+    echo -e "Done\n"
 }
 cleanup_function() {
     if [ -d "$destination"/Essential ]; then
@@ -173,16 +188,16 @@ unraid_noitification_function() {
 discord_function() {
     get_ts=$(date -u -Iseconds)
     if [ "$full_backup" == "no" ]; then
-        if [ "$cf" = false ]; then
+        if [ "$cf" == false ]; then
             curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${bot_name}"'","embeds": [{"title": "Plex Backup","description": "Essential Plex data has been backed up.","fields": [{"name": "Runtime:","value": "'"${run_output}"'"},{"name": "'"This Essential backup size:"'","value": "'"${essential_size}"'"},{"name": "Total size of all Essential backups:","value": "'"$(du -sh "$dest/Essential/" | awk '{print $1}')"'"},{"name": "Days since last Full backup","value": "'"${days}"'"}],"footer": {"text": "Powered by: Drazzilb | Blunt pencils are really pointless.","icon_url": "https://i.imgur.com/r69iYhr.png"},"color": "'"${bar_color}"'","timestamp": "'"${get_ts}"'"}]}' "$webhook"
-            echo -e "Discord notification sent.\n"
+            echo -e "Discord notification sent.1\n"
         else
             curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${bot_name}"'","embeds": [{"title": "Plex Backup","description": "Essential & Full Plex data has been backed up.","fields": [{"name": "Runtime:","value": "'"${run_output}"'"},{"name": "'"This Essential backup size:"'","value": "'"${essential_size}"'"},{"name": "'"This Full Backup:"'","value": "'"${full_size}"'"},{"name": "Total size of all Essential backups:","value": "'"$(du -sh "$dest/Essential/" | awk '{print $1}')"'"},{"name": "'"Total size of all Full backups:"'","value": "'"$(du -sh "$dest/Full/" | awk '{print $1}')"'"},{"name": "Days since last Full backup","value": "'"${days}"'"}],"footer": {"text": "Powered by: Drazzilb | Fighting for peace is like screwing for virginity.","icon_url": "https://i.imgur.com/r69iYhr.png"},"color": "'"${bar_color}"'","timestamp": "'"${get_ts}"'"}]}' "$webhook"
-            echo -e "Discord notification sent.\n"
+            echo -e "Discord notification sent.2\n"
         fi
     else
         curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${bot_name}"'","embeds": [{"title": "Plex Backup","description": "Full Plex data has been backed up.","fields": [{"name": "Runtime:","value": "'"${run_output}"'"},{"name": "'"This Full Backup:"'","value": "'"${full_size}"'"},{"name": "'"Total size of all Full backups:"'","value": "'"$(du -sh "$dest/Full/" | awk '{print $1}')"'"},{"name": "Days since last Full backup","value": "'"${days}"'"}],"footer": {"text": "'"Powered by: Drazzilb | I buy all my guns from a guy called T-Rex. He's a small arms dealer."'","icon_url": "https://i.imgur.com/r69iYhr.png"},"color": "'"${bar_color}"'","timestamp": "'"${get_ts}"'"}]}' "$webhook"
-        echo -e "Discord notification sent.\n"
+        echo -e "Discord notification sent.3\n"
     fi
 }
 debug_output_function() {
@@ -196,21 +211,23 @@ debug_output_function() {
     echo -e "get_ts: $get_ts"
     echo -e "Webhook: $webhook"
     echo -e "lastbackup: $lastbackup"
+    echo -e "basename: $source_basename"
 }
 main() {
     start=$(date +%s) # start time of script for statistics
     error_handling_function
     script_setup_function
+    cf=false
     if [ "$full_backup" == "no" ]; then
+        days=$((($(date --date="$date" +%s) - $(date --date="$lastbackup" +%s)) / (60 * 60 * 24)))
         # Essential Backup
-        backup_function "tar -cf $dest/Essential/$dt/Essential_Plex_Data_Backup-$now.tar "Plug-in Support/Databases" "Plug-in Support/Preferences" Preferences.xml" "tar -cf $dest/Essential/$dt/Essential_Plex_Data_Backup.tar "Plug-in Support/Databases" "Plug-in Support/Preferences" Preferences.xml" "Essential"
+        backup_function "Essential"
         essential_size=$size
         if [ "$force_full_backup" != 0 ]; then
-            days=$((($(date --date="$date" +%s) - $(date --date="$lastbackup" +%s)) / (60 * 60 * 24)))
             # If days is greater than force full backup time OR  last backup hasn't happened
             if [[ "$days" -gt $force_full_backup ]] || [[ "$lastbackup" == 0 ]]; then
                 cf=true # True == Full && Essential / cf == create full
-                backup_function  "tar --exclude=$source/Cache --exclude=$source/Codecs -cf $dest/Full/$dt/Full_Plex_Data_Backup-$now.tar $source" "tar --exclude=$source/Cache --exclude=$source/Codecs -cf $dest/Full/$dt/Full_Plex_Data_Backup.tar $source" "Full"
+                backup_function "Full"
                 full_size=$size
                 if [ $debug == yes ]; then
                     date >"$cwd"/tmp_last_backup.tmp
@@ -222,8 +239,10 @@ main() {
                 echo -e "Last Full backup created $days days ago... skipping\n"
             fi
         fi
+        cf=false
     else # full_backup == yes, Creates full backup regardless, no essential backup created
-        backup_function "tar --exclude=$source/Cache --exclude=$source/Codecs -cf $dest/Full/$dt/Full_Plex_Data_Backup-$now.tar $source" "tar --exclude=$source/Cache --exclude=$source/Codecs -cf $dest/Full/$dt/Full_Plex_Data_Backup.tar $source" "Full"
+        cf=true
+        backup_function "Full"
         full_size=$size
         if [ $debug == yes ]; then
             date >"$cwd"/tmp_last_backup.tmp
@@ -250,4 +269,3 @@ main() {
     rm "/tmp/i.am.running.plex.tmp"
 }
 main
-exit

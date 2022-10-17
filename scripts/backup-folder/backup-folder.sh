e9618c327b3efb29d32175d569e48ffc3d27b1e9
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2022-10-17 09:02:39 -0700

    v2.3.8
    - Removed PIGZ requirement for compressed backups
    - Added usage of 7Zip for compression
    - Renamed certain variables
    - Removed requirement of no trialing slash on path

--- scripts/backup-folder/backup-folder.sh
+++ scripts/backup-folder/backup-folder.sh
@@ -8,6 +8,8 @@
 #      |____/ \__,_|\___|_|\_\\__,_| .__/  |_|  \___/|_|\__,_|\___|_|
 #                                  | |
 #                                  |_|
+# v2.3.8
+
 # Purpose:
 # The purpose of this script is to backup a defined folder to another folder using tar archiving and pigz compression
 # This can be useful if you have folders that are vital to keep and wish to back them up to another location
@@ -15,27 +17,25 @@
 # at all times of a particular picture. Backups are deleted at intervals as new ones become available. The offsite backup is kept in sync
 # with the backup share.
 # There are many different ways to achieve this functionality I wrote this to suite my needs.
+
 #------------- DEFINE VARIABLES -------------#
-name=''            # Set your script name, must be unique to any other script.
-source=''          # Set source directory
-destination=''     # Set backup directory
-keep_backup=2     # Number of backups to keep 
-use_pigz=yes       # Use pigz to further compress your backup (yes) will use pigz to further compress, (no) will not use pigz
-pigz_compression=9 # Define compression level to use with pigz
-# 0 = No compression
-# 1 = Least compression/Fastest
-# 6 = Default compression/Default Speed
-# 9 = Maximum Compression/Slowest
-unraid_notify=yes   # Use unRAID's built in notification system
-alternate_format=no #This option will remove the time from the file and move it over to the directory structure.
-#Yes = /path/to/source/yyyy-mm-dd@00.01_AM/<container_name>.tar.gz
-#No = /path/to/source/yyyy-mm-dd/<container_name>-12_01_AM.tar.gz
+name=''                         # Set your script name, must be unique to any other script.
+source=''                       # Set source directory
+destination=''                  # Set backup directory
+keep_backup=2                   # Number of days to keep backup
+compress=yes                    # Use 7Zip to further compress your backup (yes) will use 7Zip to further compress, (no) will not use 7Zip
+
+unraid_notify=no                # Use unRAID's built in notification system
+alternate_format=no             #This option will remove the time from the file and move it over to the directory structure.
+                                    #Yes = /path/to/source/yyyy-mm-dd@00.01_AM/<container_name>.tar
+                                    #No = /path/to/source/yyyy-mm-dd/<container_name>-12_01_AM.tar.7z
 #------------- DEFINE DISCORD VARIABLES -------------#
 # This section is not required
-use_discord=yes             # Use discord for notifications
-webhook=''                  # Discord webhook
-bot_name='Notification Bot' # Name your bot
-bar_color='15048717'        # The bar color for discord notifications, must be Decimal code                                                                                                        # The bar color for discord notifications, must be decimal -> https://www.mathsisfun.com/hexadecimal-decimal-colors.html
+
+use_discord=yes                                                                                                                    # Use discord for notifications
+webhook=''                      # Discord webhook
+bot_name='Notification Bot'                                                                                                        # Name your bot
+bar_color='3036236'                                                                                                                # The bar color for discord notifications, must be decimal -> https://www.mathsisfun.com/hexadecimal-decimal-colors.html
 
 #------------- DO NOT MODIFY BELOW THIS LINE -------------#
 # Will not run again if currently running.
@@ -52,10 +52,12 @@ if [ "$use_discord" == "yes" ] && [ -z "$webhook" ]; then
     exit 1
 fi
 
-command -v pigz >/dev/null 2>&1 || {
-    echo -e "pigz is not installed.\nPlease install pigz and rerun.\nIf on unRaid, pigz can be found through the NerdPack which is found in the appstore" >&2
-    exit 1
-}
+if [ "$compress" == "yes" ]; then
+    command -v 7z >/dev/null 2>&1 || {
+        echo -e "7Zip is not installed.\nPlease install 7Zip and rerun.\nIf on unRaid, 7Zip can be found through the NerdTools which is found in the appstore" >&2
+        exit 1
+    }
+fi
 
 if [ -e "/tmp/i.am.running.${name}.tmp" ]; then
     echo "Another instance of the script is running. Aborting."
@@ -67,8 +69,13 @@ fi
 
 #Set variables
 start=$(date +%s) #Sets start time for runtime information
-cd "$(realpath -s "$source")" || exit
-dest=$(realpath -s "$destination")/
+
+parentdir=$(dirname "$source")
+cd "$(realpath -s "$parentdir")" || exit
+source_basename=$(basename "$source")
+
+dest=$(realpath -s "$destination")
+
 if [ "$alternate_format" == "yes" ]; then
     dt=$(date +"%Y-%m-%d"@%H.%M)
 else
@@ -83,24 +90,24 @@ echo -e "\nCreating backup..."
 mkdir -p "$dest/$dt"
 
 # Data Backup
-if [ "$alternate_format" != "yes" ]; then
-    tar -cf "$dest/$dt/backup-$now.tar" "$source"
-    run_size=$(du -sh "$dest/$dt/backup-$now.tar" | awk '{print $1}') #Set run_size information
-    if [ $use_pigz == yes ]; then
-        echo -e "\nUsing pigz to compress backup... this could take a while..."
-        pigz -$pigz_compression "$dest/$dt/backup-$now.tar"
-        run_size=$(du -sh "$dest/$dt/backup-$now.tar.gz" | awk '{print $1}')
+if [ "$compress" == "yes" ]; then
+    if [ "$alternate_format" != "yes" ]; then
+        tar cf - "$source_basename" | 7z a -si -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "$dest/$dt/backup-$now.tar.7z"
+        run_size=$(du -sh "$dest/$dt/backup-$now.tar.7z" | awk '{print $1}')
+    else
+
+        tar cf - "$source_basename" | 7z a -si -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "$dest/$dt/backup.tar.7z"
+        run_size=$(du -sh "$dest/$dt/backup.tar.7z" | awk '{print $1}')
     fi
 else
-    tar -cf "$dest/$dt/backup-$now.tar" "$source"
-    run_size=$(du -sh "$dest/$dt/backup.tar" | awk '{print $1}') #Set run_size information
-    if [ $use_pigz == yes ]; then
-        echo -e "\nUsing pigz to compress backup... this could take a while..."
-        pigz -$pigz_compression "$dest/$dt/backup.tar"
-        run_size=$(du -sh "$dest/$dt/backup.tar.gz" | awk '{print $1}')
+    if [ "$alternate_format" != "yes" ]; then
+        tar -cf "$dest/$dt/backup-$now.tar" "$source"
+        run_size=$(du -sh "$dest/$dt/backup-$now.tar" | awk '{print $1}') #Set run_size information
+    else
+        tar -cf "$dest/$dt/backup-$now.tar" "$source"
+        run_size=$(du -sh "$dest/$dt/backup.tar" | awk '{print $1}') #Set run_size information
     fi
 fi
-
 sleep 2
 chmod -R 777 "$dest"
 
@@ -137,10 +144,10 @@ fi
 
 # Notifications
 if [ "$unraid_notify" == "yes" ]; then
+    get_ts=$(date -u -Iseconds)
     /usr/local/emhttp/webGui/scripts/notify -e "Unraid Server Notice" -s "${name} Backup" -d "Backup completed: ${name} data has been backed up." -i "normal"
 fi
 if [ "$use_discord" == "yes" ]; then
-    get_ts=$(date -u -Iseconds)
     curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${bot_name}"'","embeds": [{"title": "'"${name} Backup Complete"'","description": "'"Backup completed: ${name} data has been backed up."'","fields": [{"name": "Runtime:","value": "'"${run_output}"'"},{"name": "'"This Backup's size:"'","value": "'"${run_size}"'"},{"name": "Total size of all backups:","value": "'"${total_size}"'"}],"footer": {"text": "Powered by: Drazzilb | What if there were no hypothetical questions?","icon_url": "https://i.imgur.com/r69iYhr.png"},"color": "'"${bar_color}"'","timestamp": "'"${get_ts}"'"}]}' "$webhook"
     echo -e "\nDiscord notification sent."
 fi
@@ -148,5 +155,3 @@ echo -e '\nAll Done!\n'
 # Removing temp \file
 rm "/tmp/i.am.running.${name}.tmp"
 exit 0
-#
-# v1.3.8

41a6af7c70cb0b4212c5f66a0ed2dcfe51e7f600
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2023-01-13 14:10:41 -0800

    Major Overhaul
    Major overhaul of the noHL Lister script. You can find more information in the README.

--- scripts/misc-scripts/noHL.sh
+++ scripts/misc-scripts/noHL.sh
@@ -7,91 +7,249 @@
 #  | | | | (_) | |  | | |____  | |____| \__ \ ||  __/ |
 #  |_| |_|\___/|_|  |_|______| |______|_|___/\__\___|_|
 
-#------------- DEFINE VARIABLES -------------#
-# Purpose:
-# The purpose of this script is to monitor your media directory for media that isn't hardlinked
-# I take great pride in seeding my entire media directory as best as I can. I wrote this to notify me if something
-# has been removed from a tracker and thus removed from my client and has left something w/in my media dir
-# without a hardlink. If the tracker thought what was there was worth deleting, then I think that it doesn't deserve
-# a place w/in my media directory. I then download another version to ensure I'm seeding the best as I can for 100% of my library
-# Please not that for this to work as well as it is intended to work. It requires your files to be named in accordance to TRaSH's guides. https://trash-guides.info
-# However this will still work without the discord output with any naming scheme if you use the log_file option
+# Usage: nohl.sh [--bot-name] [--bar-color] [--use-discord] [--help]
+# This script monitors your media directory for media that isn't hardlinked
+# Options:
+# --bot-name      : Set the bot name for discord notifications (default: Notification Bot)
+# --bar-color     : Set the bar color for discord notifications supports Hex or Decimal colors (default: 16776960)
+# --use-discord   : Set to true to use discord notifications, false to not use (default: false)
+# --help          : Show this help message
 
-source=''   # Where your media top directory is located. Eg: '/mnt/user/data/media/'
-log_file='' # Place to put your log file (not required).
-# This can be useful if you have a large list of items that aren't linked and not all show up on Discord.
-include=(# List directories to include in search no slashes
+# Define variables
+source='/mnt/user/data/media'
+log_file='/mnt/user/data/scripts/logs/'
+include=(
+    "anime series"
+    "anime movies"
+    "animated series"
+    "daily series"
+    "documentary movies"
+    "documentary series"
+    "french movies"
+    "french series"
+    "reality series"
+    "series"
     "movies"
-    "4k movies"
-    "tv shows"
-    "4k tv shows"
+    "childrens series"
 )
+use_discord=false
+webhook='https://discord.com/api/webhooks/994818363169722418/e_Zd3ViJpuHMpbq7Chm5MJE585T_rr0Iue04xksI1Glb28d1UlU-7aqG6-3wXGMVXbci'
+bar_color=16776960
+bot_name='Notification Bot'
 
-#------------- DEFINE DISCORD VARIABLES -------------#
-# This section is not required
-use_discord=yes             # Use discord for notifications
-webhook=''                  # Discord webhook
-bot_name='Notification Bot' # Name your bot
-bar_color='16776960'        # The bar color for discord notifications, must be decimal
+# Function to display help
+display_help() {
+    echo "Usage: $0 [--bot-name] [--bar-color] [--use-discord] [--help]"
+    echo "This script monitors your media directory for media that isn't hardlinked"
+    echo "Options:"
+    echo "--bot-name      : Set the bot name for discord notifications (default: Notification Bot)"
+    echo "--bar-color     : Set the bar color for discord notifications supports Hex or Decimal colors (default: 16776960)"
+    echo "--use-discord   : Set to true to use discord notifications, false to not use (default: false)"
+    echo "--help          : Show this help message"
+}
 
-#------------- DO NOT MODIFY BELOW THIS LINE -------------#
-# Improper configuration handling
-if [ ! -d "$source" ]; then
-    echo "ERROR: Your source directory does not exist, please check your configuration"
-    exit
-fi
-if [ -z "$source" ]; then
-    echo "ERROR: Your source directory is not set , please check your configuration"
-    exit
-fi
-if [ -n "${log_file}" ] && [ ! -d "$log_file" ]; then
-    echo "ERROR: log_file set but does not exist, please check your configuration"
-    exit
-fi
-if [ "$use_discord" == "yes" ] && [ -z "$webhook" ]; then
-    echo "ERROR: You're attempting to use the Discord integration but did not enter the webhook url."
-    exit
-fi
-if [ -z "${log_file}" ]; then # If source is NULL
-    echo "log_file not set Using only tmp directory, tmp file will be removed after run."
-    touch "/tmp/nohl.tmp"
-else
-    echo "log_file set to ${log_file} a log file of this run will be located there."
-    if [ -f "$log_file/nohl.log" ]; then
-        rm "${log_file}/nohl.log" # Remove previous run's tmp file
-    fi
-    touch "/tmp/nohl.tmp" "${log_file}/nohl.tmp"
-    echo "Below is a list of all files from your $source directory that do not have hardlinks" | tee -a "${log_file}/nohl.tmp" >/dev/null
-fi
-source="${source%/}"
-log_file="${log_file%/}"
-get_ts=$(date -u -Iseconds)
-# Begin search loop
-echo "Searching, please wait..."
-for ((i = 0; i < ${#include[@]}; i++)); do
-    echo "Searching ${include[$i]}..." | tee -a "${log_file}/nohl.tmp"
-    if [ -z "$log_file" ]; then
-        find "${source}/${include[$i]}" -type f -not -path "*/Scenes/*" -not -path "*/Behind The Scenes/*" -not -path "*/Trailers/*" -not -path "*/Shorts/*" -not -path "*/Featurettes/*" -not -path "*/Other/*" \( -iname \*.mp4 -o -iname \*.mkv ! -iname "*gag*" ! -iname "*blooper*" ! -iname "*Outtake*" \) -links 1 -printf "%f\n" | awk -F"[" '{print $1}' | sed $'s/[^[:print:]\t]//g' | tee -a /tmp/nohl.tmp
+# Function to convert hex to decimal
+hex_to_decimal() {
+    if [[ $1 =~ ^\#[0-9A-Fa-f]{6}$ ]]; then
+        bar_color=$((0x${1:1}))
+    elif [[ $1 =~ ^[0-9A-Fa-f]{6}$ ]]; then
+        bar_color=$((0x$1))
+    elif [[ $1 =~ ^[0-9]+$ ]]; then
+        bar_color=$1
     else
-        find "${source}/${include[$i]}" -type f -not -path "*/Scenes/*" -not -path "*/Behind The Scenes/*" -not -path "*/Trailers/*" -not -path "*/Shorts/*" -not -path "*/Featurettes/*" -not -path "*/Other/*" \( -iname \*.mp4 -o -iname \*.mkv ! -iname "*gag*" ! -iname "*blooper*" ! -iname "*Outtake*" \) -links 1 -print | tee -a "$log_file"/nohl.tmp >/dev/null
-        find "${source}/${include[$i]}" -type f -not -path "*/Scenes/*" -not -path "*/Behind The Scenes/*" -not -path "*/Trailers/*" -not -path "*/Shorts/*" -not -path "*/Featurettes/*" -not -path "*/Other/*" \( -iname \*.mp4 -o -iname \*.mkv ! -iname "*gag*" ! -iname "*blooper*" ! -iname "*Outtake*" \) -links 1 -printf "%f\n" | awk -F"[" '{print $1}' | sed $'s/[^[:print:]\t]//g' | tee -a /tmp/nohl.tmp
+        echo -e "Invalid color format, please provide a valid hex or decimal color.\nIf you provided hex with the # symbol remove or escape it with a backslash and re retry"
+        exit 1
     fi
+}
+
+# Parse command line arguments
+while [ "$1" != "" ]; do
+    case $1 in
+    --use-discord)
+        use_discord=true
+        ;;
+    --bot-name)
+        shift
+        bot_name=$1
+        ;;
+    --bar-color)
+        shift
+        hex_to_decimal "$1"
+        ;;
+    --help)
+        display_help
+        exit 0
+        ;;
+    *)
+        echo "Invalid argument $1"
+        echo "Type --help to see a list of commands"
+        exit 1
+        ;;
+    esac
+    shift
 done
-echo -e "\nSearch complete. $(sed '/^\s*$/d' /tmp/nohl.tmp | wc -l) items not hardlinked\n"
-sed 's/[0-9] - [A-Za-z].*//g'
-# Send information to Discord
-if [ "$use_discord" == "yes" ]; then
-    if [ "$(sed '/^\s*$/d' /tmp/nohl.tmp | wc -l)" -ge 1 ]; then
-        curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${bot_name}"'","embeds": [{"title": "Media files not hardlinked","description": "'"**List of media files that are not hardlinked:**\n\`\`\`$(sed -e '/^\s*$/d' -e $'s/- [[:alpha:]][[:alpha:]].*//g' -e $'s/- [[:digit:]][[:digit:]][[:digit:]].*//g' /tmp/nohl.tmp | jq -Rs . | cut -c 2- | rev | cut -c 2- | rev)\`\`\`"'","fields": [{"name": "Number of issues:","value": "'"$(sed -e'/^\s*$/d' /tmp/nohl.tmp | wc -l)"'"}],"footer": {"text": "Powered by: Drazzilb | Never trust atoms; they make up everything.","icon_url": "https://i.imgur.com/r69iYhr.png"},"color": "'"${bar_color}"'","timestamp": "'"${get_ts}"'"}]}' "$webhook"
-        echo -e "Discord notification sent"
-    else
-        curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'"${bot_name}"'","embeds": [{"title": "Media files not hardlinked","description": "'"List of media files that are not hardlinked:\n\nNo results found...\n Great job everything is hardlinked and seeding"'","fields": [{"name": "Number of issues:","value": "'"$(sed '/^\s*$/d' /tmp/nohl.tmp | wc -l)"'"}],"footer": {"text": "Powered by: Drazzilb | Two fish are in a tank. One says, How do you drive this thing?","icon_url": "https://i.imgur.com/r69iYhr.png"},"color": "'"${bar_color}"'","timestamp": "'"${get_ts}"'"}]}' "$webhook"
-        echo -e "No results found...\n Great job everything is hardlinked and seeding"
+
+# Define functions
+
+# Function to check for proper configuration
+check_config() {
+    if [[ ${#include[@]} -eq 0 ]]; then
+        echo -e "ERROR: Your list of directories is empty, please add at least one folder under the source folder to search for.\nSuch as movies or tv shows."
+        exit 1
     fi
-fi
-echo "Removing tmp files"
-rm "/tmp/nohl.tmp"
-echo -e "\nAll done!\n"
-exit
+    if [ ! -d "$source" ]; then
+        echo "ERROR: Your source directory does not exist, please check your configuration"
+        exit
+    fi
+    if [ -z "$source" ]; then
+        echo "ERROR: Your source directory is not set, please check your configuration"
+        exit
+    fi
+    if [ -n "${log_file}" ] && [ ! -d "$log_file" ]; then
+        echo "ERROR: log_file set but the directory does not exist, please check your configuration"
+        exit
+    fi
+    if [ -n "${log_file}" ] && [ -f "$log_file/nohl.log" ]; then
+        rm -f "$log_file/nohl.log" # remove log file before each run
+    fi
+    if [ -z "$webhook" ]; then
+        read -p "Please enter your Discord webhook url: " webhook
+        if [ -z "$webhook" ]; then
+            echo "ERROR: No webhook provided, please enter a valid webhook url"
+            exit 1
+        else
+            sed -i "s|webhook='https://discord.com/api/webhooks/994818363169722418/e_Zd3ViJpuHMpbq7Chm5MJE585T_rr0Iue04xksI1Glb28d1UlU-7aqG6-3wXGMVXbci'|webhook='$webhook'|" "$0"
+        fi
+    fi
+}
+
+# Function to check for hardlinks
+
+check_hardlinks() {
+    echo "Starting Search..."
+    for ((i = 0; i < ${#include[@]}; i++)); do
+        echo "****** Searching ${include[$i]}... ******" | tee -a ${log_file}/nohl.log
+        find "${source}/${include[$i]}" -type f -links 1 -iname '*' -printf "%f\n" | tee -a ${log_file}/nohl.log
+        find "${source}/${include[$i]}" -type f -links 1 -iname '*' -printf "%f\n" | awk -F"[" '{print $1}' | sed $'s/[^[:print:]\t]//g' | tee -a /tmp/nohl.tmp >/dev/null
+    done
+}
+
+# Function to send discord notification
+
+send_discord_notification() {
+    joke=$(curl -s https://raw.githubusercontent.com/Drazzilb08/userScripts/dev/jokes.txt | shuf -n 1)
+    get_ts=$(date -u -Iseconds)
+    if [ "$use_discord" == true ]; then
+        number_of_issues=0
+        if [ -f "/tmp/nohl.tmp" ]; then
+            number_of_issues=$(sed -e'/^\s*$/d' /tmp/nohl.tmp | wc -l)
+            list_of_issues=$(sed -e '/^\s*$/d' -e 's/\(.*\) - S\([0-9][0-9]\)E\([0-9][0-9]\) - [0-9]* -.*/\1 - S\2E\3/' -e 's/\(.*\) {\(.*\)}/\1/' /tmp/nohl.tmp | jq -Rs . | cut -c 2- | rev | cut -c 2- | rev)
+        fi
+        generate_payload
+        curl -s -H "Content-Type: application/json" -X POST -d "$payload" "$webhook"
+
+    fi
+    rm -f /tmp/nohl.tmp
+}
+generate_payload() {
+    if [ "$number_of_issues" -lt 50 ]; then
+        if [ "$bar_color" == 16776960 ]; then
+            bar_color=16776960
+        fi
+        payload='{
+            "username": "'"${bot_name}"'",
+            "embeds":
+            [
+                {
+                    "title": "Some media files not hardlinked",
+                    "description": "'"**List of media files that are not hardlinked:**\n\`\`\`$list_of_issues\`\`\`"'",
+                    "fields":
+                    [
+                        {
+                            "name": "Number of Issues:",
+                            "value": "'"$number_of_issues"'"
+                        }
+                    ],
+                    "footer":
+                    {
+                        "text": "'"Powered by: Drazzilb | ${joke}"'",
+                        "icon_url": "https://i.imgur.com/r69iYhr.png"},
+                        "color": "'"${bar_color}"'",
+                        "timestamp": "'"${get_ts}"'"
+                }
+            ]
+        }'
+
+    fi
+    if [ "$number_of_issues" -gt 50 ]; then
+        if [ "$bar_color" == 16776960 ]; then
+            bar_color=16711680
+        fi
+        payload='{
+            "username": "'"${bot_name}"'",
+            "embeds":
+            [
+                {
+                    "title": "A lot of Media files not hardlinked",
+                    "description": "'"Your media files has been scanned to determine what media files do not have hardlinks"'",
+                    "fields":
+                    [
+                        {
+                            "name": "Number of Issues:",
+                            "value": "'"$number_of_issues"'"
+                            },
+                        {
+                            "name": "Warning:",
+                            "value": "'"You have $number_of_issues.\n\nSince you have so many and Discord only allows so many lines to be sent at once, the normal list has been removed.\n\nPlease refer to your logfile to determine what files nolonger have hardlinks."'"
+                        }
+                    ],
+                    "footer":
+                    {
+                        "text": "'"Powered by: Drazzilb | ${joke}"'",
+                        "icon_url": "https://i.imgur.com/r69iYhr.png"},
+                        "color": "'"${bar_color}"'",
+                        "timestamp": "'"${get_ts}"'"
+                }
+            ]
+        }'
+    fi
+    if [ "$number_of_issues" -eq 0 ]; then
+        if [ "$bar_color" == 16776960 ]; then
+            bar_color=65280
+        fi
+        payload='{
+            "username": "'"${bot_name}"'",
+            "embeds": 
+            [
+                {
+                    "title": "All media files are hardlinked",
+                    "description": "'"List of media files that are not hardlinked:\n\`\`\`No results found...\`\`\`\nGreat job everything is hardlinked.\n"'",
+                    "fields": 
+                    [
+                        {
+                            "name": "Number of issues:","value": "'"$number_of_issues"'"
+                        }
+                    ],
+                    "footer": 
+                    {
+                        "text": "'"Powered by: Drazzilb | ${joke}"'",
+                        "icon_url": "https://i.imgur.com/r69iYhr.png"},
+                        "color": "'"${bar_color}"'",
+                        "timestamp": "'"${get_ts}"'"}]}'
+        echo -e "No results found...\nGreat job everything is hardlinked and seeding"
+    fi
+}
+# Main function
+
+main() {
+    check_config
+    check_hardlinks
+    send_discord_notification
+}
+
+# Call main function
+
+main
+
 #
-# v 1.1.4
+# v 2.0.0

98399774426f16e8b491ab6dfb38974d0704f08a
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2023-01-24 09:59:31 -0800

    Update backup-plex.sh

--- scripts/backup-plex/backup-plex.sh
+++ scripts/backup-plex/backup-plex.sh
@@ -17,12 +17,10 @@ config_file=""
 debug=false # Only use if you want to see the final output of every variable.
 
 config_file() {
-
     if [ -z "$config_file" ]; then
         script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
         config_file="$script_dir/backup-plex.conf"
     fi
-
     # Check if config file exists
     if [ -f "$config_file" ]; then
         # Read config file

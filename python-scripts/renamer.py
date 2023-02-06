02c9bf0b0bddfb3cdfd4d5d9261692f8402829c4
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2023-02-06 08:50:49 -0800

    code cleanup

--- python-scripts/renamer.py
+++ python-scripts/renamer.py
@@ -45,41 +45,37 @@ collection_threshold=99
 
 file_list = sorted(os.listdir(source_dir))
 
-def get_INFO(INFO_type):
+def get_info(info_type):
     # Define headers with Content-Type and Radarr API Key
     headers = {'Content-Type': 'application/json', }
     headers['x-api-key'] = radarr_api_key
-    
-    # Get INFOrmation about collections or movies, depending on INFO_type
+    # Get information about collections or movies, depending on info_type
     try:
-        if INFO_type == "collections":
+        if info_type == "collections":
             response = requests.get(radarr_url + '/api/v3/collection', headers=headers)
-        elif INFO_type == "movies":
+        elif info_type == "movies":
             response = requests.get(radarr_url + '/api/v3/movie', headers=headers)
-        
         # Raise error if the status is not successful
         response.raise_for_status()
-        
         # Log the success of connecting to Radarr and getting informationrmation
-        logger.critical(f"Connected to Radarr.. Getting informationrmation about: {INFO_type}")
-        
+        logger.critical(f"Connected to Radarr.. Getting informationrmation about: {info_type}")
         # Return the response in JSON format
         return response.json()
     except requests.exceptions.RequestException as err:
         # Log the error while connecting to Radarr
-        logger.critical(f"Error connecting to Radarr ({INFO_type}): ", err)
+        logger.critical(f"Error connecting to Radarr ({info_type}): ", err)
         return None
 
-def get_series_INFO(sonarr_api_key, sonarr_url):
+def get_series_info(sonarr_api_key, sonarr_url):
     # Define headers with Content-Type and Sonarr API Key
     headers = {'Content-Type': 'application/json', }
     headers['x-api-key'] = sonarr_api_key
-    # Get series INFOrmation
+    # Get series information
     try:
         response = requests.get(sonarr_url + '/api/v3/series', headers=headers)
         # Raise error if the status is not successful
         response.raise_for_status()
-        # Log the success of connecting to Sonarr and getting series INFOrmation
+        # Log the success of connecting to Sonarr and getting series information
         logger.critical(f"Connected to Sonarr.. Getting series information...")
         # Return the response in JSON format
         return response.json()
@@ -95,20 +91,25 @@ def get_series_INFO(sonarr_api_key, sonarr_url):
             logger.critical(f"Error connecting to Sonarr: {err}")
         return None
 
+def get_collections(baseurl, token, library_name):
+    plex = plexapi.PlexServer(baseurl, token)
+    library = plex.library.section(library_name)
+    collections = library.all()
+    collection_names = [collection.title for collection in collections]
+    return collection_names
+
 def match_series(series, file):
-    # initialize the best_match to None
-    best_match = None
-    # initialize the best_distance to the series_threshold
-    best_distance = series_threshold
     # Get the matched_series's name and year from the file
     file_name = file.split("(")[0].rstrip()
-    logger.debug(f'file_name: {file_name}')
     year = file.split("(")[1].split(")")[0]
-    logger.debug(f'year: {year}')
-    
+    year = str(year)
+    best_match = None
+    closest_match = None
+    closest_score = 0
     # loop through the series list
     for matched_series in series:
         matched_series_name = matched_series['title']
+        matched_series_name = remove_illegal_chars(matched_series_name)
         matched_series_year = matched_series['year']
         matched_series_year = str(matched_series_year)
         
@@ -117,8 +118,13 @@ def match_series(series, file):
             if year == matched_series_year:
                 best_match = matched_series
                 break
+        elif matched_series_name_match >= (series_threshold - 5):
+            if year == matched_series_year:
+                if closest_score < matched_series_name_match:
+                        closest_match = matched_series
+                        closest_year = matched_series_year
+                        closest_score = matched_series_name_match
     if best_match:
-        
         return best_match, None
     else:
         file_name_with_year = f"{file_name} ({year})"
@@ -130,55 +136,58 @@ def match_series(series, file):
                 if year == matched_series_year:
                     best_match = matched_series
                 break
+            elif matched_series_name_match >= (series_threshold - 5):
+                if year == matched_series_year:
+                    if closest_score < matched_series_name_match:
+                            closest_match = matched_series
+                            closest_year = matched_series_year
+                            closest_score = matched_series_name_match
         # if a best_match was found in the second loop
         if best_match:
             return best_match, None
+        elif closest_match:
+            return None, f"No match found, closest match for {file} was {closest_match['title']} ({closest_year}) with a score of {closest_score}"
         else:
-            # if no match was found, return None and the error message
-            return None, "No match found"
+            return None, None
 
 def match_movies(movies, file):
     # Split the file name and year from the file
     file_name = file.split("(")[0].rstrip()
     year = file.split("(")[1].split(")")[0]
-
-    # Check if the file is a TV show (not a movie)
-    if "Season" in file_name:
-        return None, "Show"
-    # Check if there are other files in the directory with the same name and the word "Season"
-    elif any(f.startswith(file_name) and "Season" in f for f in os.listdir(source_dir)):
-        return None, "Show"
-    # If it's not a TV show, try to match it to a movie
-    else:
-        best_match = None
-        for matched_movie in movies:
-            matched_movie_name = matched_movie['title']
-            matched_movie_year = matched_movie['year']
-            matched_movie_year = str(matched_movie_year)
-
-            matched_movie_name_match = fuzz.token_sort_ratio(file_name, matched_movie_name)
-            if matched_movie_name_match >= movies_threshold:
-                if year == matched_movie_year:
-                    best_match = matched_movie
-                    break
+    year = str(year)
+    best_match = None
+    closest_match = None
+    closest_score = 0
+    for matched_movie in movies:
+        matched_movie_name = matched_movie['title']
+        matched_movie_name = remove_illegal_chars(matched_movie_name)
+        matched_movie_year = matched_movie['year']
+        matched_movie_year = str(matched_movie_year)
+        matched_movie_name_match = fuzz.token_sort_ratio(file_name, matched_movie_name)
+        if matched_movie_name_match >= movies_threshold:
+            if year == matched_movie_year:
+                best_match = matched_movie
+                break
+        elif matched_movie_name_match >= (movies_threshold - 5):
+            if year == matched_movie_year:
+                if closest_score < matched_movie_name_match:
+                        closest_match = matched_movie
+                        closest_year = matched_movie_year
+                        closest_score = matched_movie_name_match
+    if best_match:
         return best_match, None
-
-        # Return the best match, or None if no match was found
-        if best_match:
-            
-            return best_match, None
-        else:
-            return None, "No match found"
+    elif closest_match:
+        return None, f"No match found, closest match for {file} was {closest_match['title']} ({closest_year}) with a score of {closest_score}"
+    else:
+        return None, None
 
 def match_collection(collection, file):
     # Split the file name and extension and get the file name only
     file_name = os.path.splitext(file)[0]
     logger.debug(f'file_name: {file_name}')
-    
     # Check if the file name contains the word "Season" or "Special", return None and "Show" if true
     if "Season" in file_name or "Special" in file_name:
         return None, "Show"
-    
     # Check if there are other files in the directory with the same name and the word "Season", return None and "Show" if true
     elif any(f.startswith(file_name) and "Season" in f for f in os.listdir(source_dir)):
         return None, "Show"
@@ -195,9 +204,8 @@ def match_collection(collection, file):
                 best_distance = matched_collection_name_match
                 best_match = matched_collection
         if best_match:
-            
             return best_match, None
-        else:
+        elif not re.search(r'\(\d{4}\).', file):
             return None, "No match found"
 
 def rename_movies(matched_movie, file, destination_dir, source_dir):
@@ -207,11 +215,9 @@ def rename_movies(matched_movie, file, destination_dir, source_dir):
     logger.debug(f"matched_movie_folder: {matched_movie_folder}")
     file_extension = os.path.basename(file).split(".")[-1]
     matched_movie_folder = matched_movie_folder + "." + file_extension
-    
     # Create the full path to the destination folder
     destination = os.path.join(destination_dir, matched_movie_folder)
     source = os.path.join(source_dir, file)
-    
     # Check if the file name is different from the matched_movie's folder name
     if os.path.basename(file) != matched_movie_folder:
         if dry_run:
@@ -240,10 +246,9 @@ def rename_series(matched_series, file, destination_dir, source_dir):
     logger.debug(f"folder_path: {folder_path}")
     matched_series_folder = os.path.basename(folder_path)
     logger.debug(f"matched_series_folder: {matched_series_folder}")
-    
     # Get the file extension of the input file
     file_extension = os.path.basename(file).split(".")[-1]
-    # Check if the file name contains "Season" or "Specials" and append the corresponding INFO to the matched series folder name
+    # Check if the file name contains "Season" or "Specials" and append the corresponding info to the matched series folder name
     if "_Season" in file:
         show_name, season_info = file.split("_Season")
         if show_name == matched_series_folder:
@@ -261,13 +266,10 @@ def rename_series(matched_series, file, destination_dir, source_dir):
             matched_series_folder = matched_series_folder + "_Season00." + file_extension
         else:
             matched_series_folder = matched_series_folder + "." + file_extension
-    
     # Set the destination path for the file to be renamed
     destination = os.path.join(destination_dir, matched_series_folder)
-    
     # Set the source path for the file
     source = os.path.join(source_dir, file)
-    
     # If the file name is not equal to the matched series folder name, then rename the file
     if os.path.basename(file) != matched_series_folder:
         if dry_run:
@@ -287,13 +289,9 @@ def rename_series(matched_series, file, destination_dir, source_dir):
             logger.critical(f"{file} -->> {matched_series_folder}")
             return 
 
-
-# def remove_illegal_chars(string):
-#     return re.sub(r'[^\w\s-]', '', string)
 def remove_illegal_chars(string):
     # Define a regular expression pattern to match illegal characters
     illegal_characters = re.compile(r'[\\/:*?"<>|\0]')
-    
     # Replace all instances of illegal characters with an empty string
     return illegal_characters.sub("", string)
 
@@ -301,21 +299,16 @@ def rename_collections(matched_collection, file, destination_dir, source_dir):
     # Get the title of the matched collection
     matched_collection_title = matched_collection['title']
     logger.debug(f"matched_collection_title: {matched_collection_title}")
-    
     # Get the file extension of the file
     file_extension = os.path.basename(file).split(".")[-1]
-    
     # Concatenate the matched collection title and file extension to form the new file name
     matched_collection_title = matched_collection_title + "." + file_extension
-    
+    # Remove any illegal characters from the matched collection name
     matched_collection_title = remove_illegal_chars(matched_collection_title)
-    
     # Get the destination path for the renamed file
     destination = os.path.join(destination_dir, matched_collection_title)
-    
     # Get the source path for the current file
     source = os.path.join(source_dir, file)
-    
     # If the current file name is not the same as the new file name
     if os.path.basename(file) != matched_collection_title:
         # If the code is in dry run mode, log the intended file rename operation
@@ -327,7 +320,6 @@ def rename_collections(matched_collection, file, destination_dir, source_dir):
             shutil.move(source, destination)
             logger.critical(f"{file} -> {matched_collection_title}")
             return
-    
     # If the current file name is the same as the new file name
     if os.path.basename(file) == matched_collection_title:
         # If the code is in dry run mode, log the intended file rename operation
@@ -362,32 +354,25 @@ def validate_input(source_dir, destination_dir, radarr_url, sonarr_url, sonarr_u
     if sonarr_url_1:
         if sonarr_url_1 and not (sonarr_url_1.startswith("http://") or sonarr_url_1.startswith("https://")):
             raise ValueError("Sonarr URL 1 must start with 'http://' or 'https://'.")
-    if log_level not in ["DEBUG", "INFO", "CRITICAL"]:
-        raise ValueError("Invalid log level. Must be one of 'DEBUG', 'INFO', 'CRITICAL'.")
 
 def main():
     validate_input(source_dir, destination_dir, radarr_url, sonarr_url, sonarr_url_1, dry_run, log_level)
-
     # Check if dry_run is set to True
     if dry_run:
-        logger.info("********************************")
-        logger.info("*         Dry_run True         *")
-        logger.info("********************************")
+        logger.critical("*************************************")
+        logger.critical("*         Dry_run Activated         *")
+        logger.critical("*************************************")
         # Log a warning message that no changes will be made
-        logger.info("****NO CHANGES WILL BE MADE*****")
+        logger.critical("*******NO CHANGES WILL BE MADE*******")
 
     # Log the destination directory
     logger.critical(f"Destination folder: {destination_dir}")
-
     # Check if both radarr_api_key and radarr_url are set
     if radarr_api_key and radarr_url:
-        logger.info("********************************")
-        logger.info("*     Connecting to Radarr     *")
-        logger.info("********************************")
-        # Get movie and collection INFOrmation from Radarr
-        movies = get_INFO("movies")
-        collection = get_INFO("collections")
-        logger.info("********************************")
+        # Get movie and collection information from Radarr
+        movies = get_info("movies")
+        collection = get_info("collections")
+        # collections = get_collections(baseurl, token, library_name)
         # Iterate through files in source_dir
         for file in tqdm(file_list, desc='Processing files', total=len(file_list)):
             # Check if the file name contains "(" or ")"
@@ -403,9 +388,6 @@ def main():
                     elif reason:
                         logger.info(f"{file} was skipped because: {reason}")
                         continue
-                    # If no reason is given, do nothing
-                    else:
-                        pass
             else:
                 # If movies are available
                 if movies is not None:
@@ -417,19 +399,12 @@ def main():
                     # If a reason is given for not finding a match, log it
                     elif reason:
                         logger.info(f"{file} was skipped because: {reason}")
-                    # If no reason is given, do nothing
-                    else:
-                        pass
     # Check if sonarr_api_key and sonarr_url are both present
     if sonarr_api_key and sonarr_url:
         # Log a header indicating the start of connecting to Sonarr
-        logger.info("********************************")
-        logger.info("*     Connecting to Sonarr     *")
-        logger.info("********************************")
-        # Get the series INFOrmation from Sonarr
-        series = get_series_INFO(sonarr_api_key, sonarr_url)
+        # Get the series information from Sonarr
+        series = get_series_info(sonarr_api_key, sonarr_url)
         # Log a header indicating the end of connecting to Sonarr
-        logger.info("********************************")
         # Loop through all the files in the source directory
         for file in tqdm(file_list, desc='Processing files', total=len(file_list)):
             # Skip files that don't contain "(" or ")" in their names
@@ -444,21 +419,11 @@ def main():
                 # If the file was skipped for a reason, log the reason
                 elif reason:
                     logger.info(f"{file} was skipped because: {reason}")
-                    pass
-                # If no match was found, log a CRITICAL error
-                else:
-                    logger.critical(f"No match found for {file}")
 
 # Check if sonarr_api_key_1 and sonarr_url_1 are both present
     if sonarr_api_key_1 and sonarr_url_1:
-        # Log a header indicating the start of connecting to Sonarr 2
-        logger.info("********************************")
-        logger.info("*    Connecting to Sonarr 2    *")
-        logger.info("********************************")
-        # Get the series INFOrmation from Sonarr 2
-        series_1 = get_series_INFO(sonarr_api_key_1, sonarr_url_1)
-        # Log a header indicating the end of connecting to Sonarr 2
-        logger.info("********************************")
+        # Get the series information from Sonarr 2
+        series_1 = get_series_info(sonarr_api_key_1, sonarr_url_1)
         # Loop through all the files in the source directory
         for file in tqdm(file_list, desc='Processing files', total=len(file_list)):
             # Skip files that don't contain "(" or ")" in their names
@@ -473,16 +438,12 @@ def main():
                 # If the file was skipped for a reason, log the reason
                 elif reason:
                     logger.info(f"{file} was skipped because: {reason}")
-                    pass
-                # If no match was found, log a CRITICAL error
-                else:
-                    logger.critical(f"No match found for {file}")
     permissions = 0o777
     os.chmod(destination_dir, permissions)
 
 def setup_logger(log_level):
     # Create a directory to store logs, if it doesn't exist
-    log_dir = "logs"
+    log_dir = os.path.dirname(os.path.realpath(__file__)) + "/logs"
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
     # Get the current date in YYYY-MM-DD format
@@ -515,11 +476,10 @@ def setup_logger(log_level):
     if log_level == 'debug':
         console_handler.setLevel(logging.DEBUG)
     elif log_level == 'info':
-        console_handler.setLevel(logging.INFO)
+        console_handler.setLevel(logging.info)
     elif log_level == 'critical':
         console_handler.setLevel(logging.CRITICAL)
     logger.addHandler(console_handler)
-
     return logger
 
 if __name__ == '__main__':

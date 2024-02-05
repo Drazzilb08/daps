02d85c0dd1e952403e63de1cfd899c72000db80a
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2024-02-04 16:02:54 -0800

    changed start up permissions

--- start.sh
+++ start.sh
@@ -19,8 +19,22 @@ cp -Rn /app/config/* /config
 # Change permissions of the /config directory to 777
 chmod -R 777 /config
 
-# Modify the user ID of the dockeruser
-chown -R dockeruser:dockeruser /app /config
+# Loop through all mounted volumes
+for volume in $(find / -mount | cut -d' ' -f3); do
+  # Skip over read-only volumes
+  if mount | grep -F "$volume" | grep -q "(ro,"; then
+    echo "Skipping read-only volume: $volume"
+    continue
+  fi
+  if [[ "$volume" =~ ^/(root|home|etc|var|boot|usr|mnt|lib|bin|sbin|lib64|proc|sys|dev|run|tmp|media|srv|opt|snap|app) ]]; then
+    continue
+  fi
+  echo "Changing ownership of $volume to $PUID:$PGID"
+  # Change ownership recursively
+  chown -R $PUID:$PGID "$volume"
+done
 
-# Change ownership of the /app and /config directory to dockeruser
+chown -R $PUID:$PGID /app
+
+# Run the command as the dockeruser
 exec runuser -u dockeruser -g dockeruser -- "$@"

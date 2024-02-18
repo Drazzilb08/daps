748752b3bce2415d1a7cdc0395f5ca2f6d0af75f
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2024-02-17 18:29:21 -0800

    simplify startup process

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 PUID=${PUID:-100}
 PGID=${PGID:-99}
 UMASK=${UMASK:-002}
-START_DEBUG=${START_DEBUG:-false}
 
 export RCLONE_CONFIG="${CONFIG_DIR}/rclone/rclone.conf"
 
@@ -35,50 +34,22 @@ umask "$UMASK"
 groupmod -o -g "$PGID" dockeruser
 usermod -o -u "$PUID" dockeruser
 
-# Copy /app/config files to CONFIG_DIR unless CONFIG_DIR == /app/config
-if [ "$CONFIG_DIR" != "/app/config" ]; then
-    # Use diff and conditional copy for files in /app/config
-    for file in /app/config/*; do
-        local_file="$CONFIG_DIR/$(basename "$file")"
-        if [ ! -f "$local_file" ] || [ "$(diff -q "$file" "$local_file")" ]; then
-            echo "Copying $(basename "$file")"
-            cp "$file" "$local_file"
-        else
-            echo "File $(basename "$file") is up to date"
-        fi
-    done
-elif [ "$CONFIG_DIR" == "/app/config" ]; then
-    # Download config files only if they differ
-    for file in config.sample.yml backup-plex-example.conf backup-appdata-example.yml; do
-        local_file="$CONFIG_DIR/$file"
-        if [ ! -f "$local_file" ] || [ "$(curl -s "https://raw.githubusercontent.com/Drazzilb08/userScripts/${BRANCH}/config/$file" | diff -q - "$local_file")" ]; then
-            echo "Downloading latest $file"
-            curl -s "https://raw.githubusercontent.com/Drazzilb08/userScripts/${BRANCH}/config/$file" -o "$local_file"
-        else
-            echo "File $file is up to date"
-        fi
-    done
-fi
+
+# Download latest config files if they don't exist or are different
+for file in config.sample.yml backup-plex-example.conf backup-appdata-example.yml; do
+    local_file="$CONFIG_DIR/$file"
+    if [ ! -f "$local_file" ] || [ "$(curl -s "https://raw.githubusercontent.com/Drazzilb08/userScripts/${BRANCH}/config/$file" | diff -q - "$local_file")" ]; then
+        echo "Downloading latest $file"
+        curl -s "https://raw.githubusercontent.com/Drazzilb08/userScripts/${BRANCH}/config/$file" -o "$local_file"
+    else
+        echo "File $file is up to date"
+    fi
+done
 
 
 echo "Starting userScripts as $(whoami) running userscripts with UID: $PUID and GID: $PGID"
 
-# Set permissions
-if [ "$START_DEBUG" = "true" ]; then
-    if ! chown -R ${PUID}:${PGID} /${CONFIG_DIR} /data /app /${log_dir}; then
-        echo "Failed to change ownership."
-        echo "DEBUG: ${PUID}:${PGID} /${CONFIG_DIR}"
-        ls -la /${CONFIG_DIR} 
-        echo "DEBUG: ${PUID}:${PGID} /data"
-        ls -la /data
-        echo "DEBUG: ${PUID}:${PGID} /app"
-        ls -la /app
-        echo "DEBUG: ${PUID}:${PGID} /${log_dir}"
-        ls -la /${log_dir}
-    fi
-else
-    chown -R ${PUID}:${PGID} /${CONFIG_DIR} /data /app > /dev/null 2>&1
-fi
+chown -R ${PUID}:${PGID} /${CONFIG_DIR} /data /app > /dev/null 2>&1
 
 # Run main.py as the dockeruser
 exec su -s /bin/bash -c "python3 /app/main.py" dockeruser

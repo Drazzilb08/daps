aa8ee725d2147c04d0eb6c0c40c5747b604a20bf
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2024-02-17 18:32:18 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -49,7 +49,7 @@ done
 
 echo "Starting userScripts as $(whoami) running userscripts with UID: $PUID and GID: $PGID"
 
-chown -R ${PUID}:${PGID} /${CONFIG_DIR} /data /app > /dev/null 2>&1
+chown -R ${PUID}:${PGID} /${CONFIG_DIR} /app > /dev/null 2>&1
 
 # Run main.py as the dockeruser
 exec su -s /bin/bash -c "python3 /app/main.py" dockeruser

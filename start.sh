1535a1b51060fe91267a0e8db7808a85dd636264
Drazzilb <65743746+Drazzilb08@users.noreply.github.com>
2024-02-19 19:52:59 -0800

    PGID:PUID Reversed

--- start.sh
+++ start.sh
@@ -50,7 +50,7 @@ done
 
 echo "Starting userScripts as $(whoami) running userscripts with UID: $PUID and GID: $PGID"
 
-chown -R ${PUID}:${PGID} /${CONFIG_DIR} /app > /dev/null 2>&1
+chown -R ${PGID}:${PUID} /${CONFIG_DIR} /app > /dev/null 2>&1
 chmod -R 777 /${CONFIG_DIR} > /dev/null 2>&1
 
 # Run main.py as the dockeruser

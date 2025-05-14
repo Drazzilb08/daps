5f29211ff049652ac08ce1cf0e4e52c304abb67e
Drazzilb <soulraver08@gmail.com>
2025-05-13 21:35:47 -0700

    small bug fix if id is in file name for collections

--- util/scanner.py
+++ util/scanner.py
@@ -133,7 +133,8 @@ def parse_file_group(folder_path: str, base_name: str, files: List[str]) -> Dict
     Returns:
         Dict: Structured media dictionary.
     """
-    title = re.sub(year_regex, '', base_name)
+    id_cleaned_name = re.sub(r"\{(?:tmdb|tvdb|imdb)-\w+\}", "", base_name).strip()
+    title = re.sub(year_regex, '', id_cleaned_name).strip()
     year = extract_year(base_name)
     tmdb_id, tvdb_id, imdb_id = extract_ids(base_name)
     normalize_title = normalize_titles(base_name)

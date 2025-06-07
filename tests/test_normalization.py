ac4506e019783306896df775e51231302b1c4cbe
Drazzilb <soulraver08@gmail.com>
2025-06-07 14:53:59 -0700

    add tests

--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -11,9 +11,22 @@ from util.normalization import normalize_titles, normalize_file_names
 # def test_preprocess_name_removes_common_words():
 #     assert preprocess_name("The Fast and the Furious") == "fastfurious"
 
+def test_normalize_titles():
+    data = [
+        "In/Spectre (2020)",
+        "The Fast and the Furious",
+        "Spider-Man: Into the Spider-Verse (2018)",
+        "Schindler’s List",
+        "Pokémon Detective Pikachu",
+        "Birds of Prey (and the Fantabulous Emancipation of One Harley Quinn) (2020)"
+    ]
+    print("\nResults for normalize_titles:")
+    for s in data:
+        print(f"{s!r} -> {normalize_titles(s)}")
 
 def test_normalize_file():
     data = [
+        "In/Spectre (2020).jpg",
         "M*A*S*H (1972) - Season 1.jpg",
         "MASH (1972) - Season 1.jpg",
         "Birds of Prey (and the Fantabulous Emancipation of One Harley Quinn) (2020).jpg",

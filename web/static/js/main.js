53a4ecd5166db115c2af871c1ab2a00f55951866
Drazzilb <soulraver08@gmail.com>
2025-07-11 08:35:01 -0700

    removed uneeded code

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,15 +1,3 @@
 // Core system scripts
-import './payload.js';
 import './navigation.js';
-import './util.js';
-import './helper.js';
-
-// Pages
 import './index.js';
-import './schedule.js';
-import './instances.js';
-import './notifications.js';
-import './poster_search.js';
-import './poster_management.js';
-import './settings/settings.js';
-import './logs.js';

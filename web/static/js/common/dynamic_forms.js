86a1cd0aa1243d0555621347688fd4c18a7f9558
Drazzilb <soulraver08@gmail.com>
2025-07-21 21:44:29 -0700

    Update dynamic_forms.js

--- web/static/js/common/dynamic_forms.js
+++ web/static/js/common/dynamic_forms.js
@@ -6,14 +6,18 @@ import { renderField } from './field_render.js';
 // -------- MAIN FORM RENDERER -------------
 export function renderSettingsForm(formFields, moduleName, config, rootConfig) {
     const schema = SETTINGS_SCHEMA.find((s) => s.key === moduleName);
+    const immediateData = config;
+    const moduleConfig = config;
     if (!schema) return;
 
     formFields.innerHTML = '';
 
     const fieldsList = document.createElement('div');
     fieldsList.className = 'settings-fields-list';
+
     schema.fields.forEach((field) => {
-        const fieldNode = renderField(field, config[field.key], config, rootConfig);
+        // At top-level, immediateData and moduleConfig are both 'config'
+        const fieldNode = renderField(field, immediateData, moduleConfig, rootConfig);
         if (fieldNode) fieldsList.appendChild(fieldNode);
     });
 

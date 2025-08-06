fc068fbf8695412e0929e8c9dc4e4ae9ebd32cf4
Drazzilb <soulraver08@gmail.com>
2025-08-05 21:49:32 -0700

    Add placeholders for media items

---
+++ ui/src/pages/MediaManage.jsx
@@ -0,0 +1,9 @@
+// import React, { useRef, useEffect, useState } from 'react';
+// import { getIcon } from '../utils/tools';
+// import {  } from '../utils/api';
+// import TooltipFactory from "../components/Tooltip";
+// import '../css/media_management.css';
+
+export default function MediaManagement() {
+    return <div>Media Management</div>;
+}
---
+++ ui/src/pages/MediaStatistics.jsx
@@ -0,0 +1,9 @@
+// import React, { useRef, useEffect, useState } from 'react';
+// import { getIcon } from '../utils/tools';
+// import {  } from '../utils/api';
+// import TooltipFactory from "../components/Tooltip";
+// import '../css/poster_management.css';
+
+export default function MediaStatistics() {
+    return <div>Media Statistics</div>;
+}

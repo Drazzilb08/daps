89df1f6c1423a7abea18cd369902de90ee838d23
Drazzilb <soulraver08@gmail.com>
2025-08-22 20:40:35 -0700

    💄 style(ui): update CSS and page components for new   architecture
      - Update header search CSS for new component structure
      - Improve popover styling consistency
      - Update MediaManage and MediaStatistics page imports
      - Align styling with new component organization

--- ui/src/pages/MediaManage.jsx
+++ ui/src/pages/MediaManage.jsx
@@ -1,5 +1,5 @@
 // import React, { useRef, useEffect, useState } from 'react';
-// import { getIcon } from '../utils/tools';
+// import { getIcon } from '../utils/uiHelpers';
 // import {  } from '../utils/api';
 // import TooltipFactory from "../components/Tooltip";
 // import '../css/pages/media-manage.css';
